3125041a4f1aea8d3797017dba8cde0be4e0dfa8
codecreator127 <codecreator127@gmail.com>
2025-11-16 16:35:47 +1300

    add terraform and github actions for deployment to aws
    github actions and terraform

---
+++ infra/main.tf
@@ -0,0 +1,151 @@
+############################
+# Existing S3 Media Bucket
+############################
+resource "aws_s3_bucket" "media_bucket" {
+  bucket = var.media_bucket_name
+}
+
+############################
+# IAM Policies (Native)
+############################
+## Assume role policy for EC2
+
+data "aws_iam_policy_document" "ec2_assume_role" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["ec2.amazonaws.com"]
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+resource "aws_iam_role" "ec2_role" {
+  name               = "backend-ec2-role"
+  assume_role_policy = data.aws_iam_policy_document.ec2_assume_role.json
+}
+
+## Policy: Allow EC2 to read/write to media bucket
+
+data "aws_iam_policy_document" "media_access" {
+  statement {
+    effect = "Allow"
+
+    actions = ["s3:GetObject", "s3:PutObject", "s3:DeleteObject"]
+    resources = ["${aws_s3_bucket.media_bucket.arn}/*"]
+  }
+}
+
+resource "aws_iam_role_policy" "media_access_policy" {
+  name   = "media-access-policy"
+  role   = aws_iam_role.ec2_role.id
+  policy = data.aws_iam_policy_document.media_access.json
+}
+
+## Instance Profile
+resource "aws_iam_instance_profile" "ec2_profile" {
+  name = "backend-ec2-profile"
+  role = aws_iam_role.ec2_role.name
+}
+
+############################
+# Security Groups
+############################
+resource "aws_security_group" "backend_sg" {
+  name        = "backend-sg"
+  description = "Allow SSH, backend traffic, and Postgres"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = var.backend_port
+    to_port     = var.backend_port
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 5432
+    to_port     = 5432
+    protocol    = "tcp"
+    cidr_blocks = ["127.0.0.1/32"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+############################
+# EC2 Backend Instance with Postgres + Backend
+############################
+resource "aws_instance" "backend" {
+  ami                         = var.ami
+  instance_type               = var.instance_type
+  subnet_id                   = var.subnet_id
+  vpc_security_group_ids      = [aws_security_group.backend_sg.id]
+  iam_instance_profile        = aws_iam_instance_profile.ec2_profile.name
+  associate_public_ip_address = true
+
+  user_data = <<-EOF
+              #!/bin/bash
+              sudo apt-get update
+              sudo apt-get install -y openjdk-17-jdk postgresql postgresql-contrib wget unzip awscli
+
+              # Create app directory
+              mkdir -p /home/ubuntu/app
+              cd /home/ubuntu/app
+
+              # Download backend artifact from S3 (if needed)
+              aws s3 cp s3://${var.artifact_bucket}/backend.jar ./backend.jar
+
+              # Start Postgres
+              sudo service postgresql start
+
+              # Set up basic Postgres database
+              sudo -u postgres psql -c "CREATE USER backend_user WITH PASSWORD 'password';"
+              sudo -u postgres psql -c "CREATE DATABASE backend_db OWNER backend_user;"
+
+              # Run backend
+              nohup java -jar backend.jar &
+              EOF
+
+  tags = {
+    Name = "backend-ec2"
+  }
+}
+
+############################
+# Outputs
+############################
+output "backend_ip" {
+  value = aws_instance.backend.public_ip
+}
+
+output "media_bucket_name" {
+  value = aws_s3_bucket.media_bucket.bucket
+}
+
+############################
+# Variables
+############################
+variable "region" {}
+variable "artifact_bucket" {}
+variable "media_bucket_name" {}
+variable "vpc_id" {}
+variable "subnet_id" {}
+variable "ami" {}
+variable "instance_type" {}
+variable "backend_port" { default = 8080 }
