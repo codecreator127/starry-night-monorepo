5905b502ba8cfdb829e66dcf271d15e3c1aec7b3
codecreator127 <codecreator127@gmail.com>
2025-11-19 18:45:57 +1300

    add s3 bucket for tf state lock and retention

--- infra/providers.tf
+++ infra/providers.tf
@@ -5,6 +5,14 @@ terraform {
       version = "6.21.0"
     }
   }
+
+  backend "s3" {
+    bucket         = "starry-night-terraform"
+    key            = "backend/terraform.tfstate"
+    region         = "ap-southeast-2"
+    dynamodb_table = "terraform-locks"
+    encrypt        = true
+  }
 }
 
 provider "aws" {
